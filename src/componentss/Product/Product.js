0fe79417ca74b179973dbb9af956bf7a9e5b5717
MD Mehede Hassan Hridoy <mdmehedehassanhridoy@gmail.com>
2022-12-03 23:28:36 +0600

    card added

--- src/componentss/Product/Product.js
+++ src/componentss/Product/Product.js
@@ -2,11 +2,21 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const{name, seller, price, stock, img, category} = props.product;
+    const{name, seller, price, stock, img, category, ratings} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className="product-info">
+              <p className='product-name'>{name}</p>
+              <p>Price : ${price}</p>
+              <p>Seller : {seller}</p>
+              <p>Stock : {stock}</p>
+              <p><small>Rating : {ratings} Stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add To Curt</p>
+            </button>
+            
         </div>
     );
 };
