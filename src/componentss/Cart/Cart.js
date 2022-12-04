e6c798306cd2764fbb3ae80c6295d64f82832810
MD Mehede Hassan Hridoy <mdmehedehassanhridoy@gmail.com>
2022-12-04 23:30:31 +0600

    ammount added

--- src/componentss/Cart/Cart.js
+++ src/componentss/Cart/Cart.js
@@ -1,11 +1,24 @@
 import React from 'react';
-
-const Cart = ({cart}) => {
+import './Cart.css'
+const Cart = (props) => {
+    const {cart} = props;
     // destriting instate of using props
+    // Calculiting total / shipping prices
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total= total + product.price;
+        shipping = shipping + product.shipping
+    }
+    const tax = (total * 0.10).toFixed(2);
     return (
-        <div>
-            <h2>This is cart container</h2>
+        <div className='cart'>
+            <h2>Order Summary</h2>
             <p>Selected Item : {cart.length}</p>
+            <p>Total price : ${total}</p>
+            <p>Total shipping : ${shipping}</p>
+            <p>Tax : ${tax}</p>
+            <h3>Grand Total : {}</h3>
         </div>
     );
 };
