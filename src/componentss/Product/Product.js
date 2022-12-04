bbce4b19dc3febef903fedf95c2609d1a300802b
MD Mehede Hassan Hridoy <mdmehedehassanhridoy@gmail.com>
2022-12-04 11:50:07 +0600

    added Icons

--- src/componentss/Product/Product.js
+++ src/componentss/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
@@ -14,7 +16,8 @@ const Product = (props) => {
               <p><small>Rating : {ratings} Stars</small></p>
             </div>
             <button onClick={() => props.handleAddToCart(props.product)}  className='btn-cart'>
-                <p>Add To Curt</p>
+                <p className='btn-text'>Add To Curt</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
             
         </div>
